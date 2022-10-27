b68e72040b647b91b4c2aa8af25904a42349524e
ye <104442379+kookoo1029@users.noreply.github.com>
2022-10-27 23:46:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 선언
+		
+		int N = Integer.parseInt(br.readLine()); // 반복횟수 입력
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=N; j>i; j--) {
+				bw.write(" "); 
+			}
+			for(int j=1; j<=i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+
+		br.close();
+		bw.flush(); // 내부 버퍼의 내용을 파일에 writer
+		bw.close();
+	}
+}
