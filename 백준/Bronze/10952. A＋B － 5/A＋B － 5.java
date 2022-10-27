8ac44605c7cba95e85cc0baec6bb1257a7cb0591
ye <104442379+kookoo1029@users.noreply.github.com>
2022-10-28 00:05:54 +0900

    [Bronze V] Title: A+B - 5, Time: 152 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,16 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 선언
 		
 		while(true) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			if(a == 0 && b == 0) break;
-			System.out.println(a + b);
+			StringTokenizer st = new StringTokenizer(br.readLine()); // 너가 입력하는 값을 끊을거야
+			int a = Integer.parseInt(st.nextToken()); // 첫번째 입력값은 a에
+			int b = Integer.parseInt(st.nextToken()); // 두번째 입력값은 b에
+			if(a==0 && b==0) {
+				break;
+			}
+			bw.write((a+b) + "\n");
 		}
+		
+		br.close();
+		bw.flush(); // 내부 버퍼의 내용을 파일에 writer
+		bw.close();
 	}
 }
