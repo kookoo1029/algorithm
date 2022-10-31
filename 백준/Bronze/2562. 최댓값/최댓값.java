548a3926872ab685044a5339f94492bd8c8278d4
ye <104442379+kookoo1029@users.noreply.github.com>
2022-11-01 02:43:00 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,15 +1,18 @@
-import java.lang.reflect.Array;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int[] arr=new int[9];
-		
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[9];
 		for (int i = 0; i < 9; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
 		int max = Arrays.stream(arr).max().getAsInt();
@@ -21,5 +24,8 @@ public class Main {
 			}
 		}
 		
-	}
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
