6111a7c6be7e682ec800edd1f810c235ee44f831
ye <104442379+kookoo1029@users.noreply.github.com>
2022-11-01 00:59:55 +0900

    [Bronze V] Title: X보다 작은 수, Time: 200 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 공백 단위로 읽을 수 있는 라인 추가
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine(), " "); // 공백 단위로 읽을 수 있는 라인 또 추가
+        
+        for (int i = 0; i < N; i++) {
+            int inputNums = Integer.parseInt(st.nextToken());
+ 
+            if (inputNums < X) {
+                bw.write(inputNums + " ");
+            }
+        }
 		
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int X = sc.nextInt();
-		int[] arr = new int[N];
-		
-		for(int i=0; i<N; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		for(int i=0; i<N; i++) {
-			if(arr[i]<X) {
-				System.out.print(arr[i] + " ");
-			}
-		}
-	}
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
