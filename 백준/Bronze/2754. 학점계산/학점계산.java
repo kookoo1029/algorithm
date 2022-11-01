6a343b3438a67468d7b7d659513b9de5a20e6d86
ye <104442379+kookoo1029@users.noreply.github.com>
2022-11-02 01:56:25 +0900

    [Bronze V] Title: 학점계산, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String[] stage = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D+", "D0", "D-", "F"};
+		double[] score = {4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0};
+		String str = sc.nextLine();
+		for(int i=0; i<stage.length; i++) {
+			if(str.equals(stage[i])) {
+				System.out.println(score[i]);
+			}
+		}
+	}
+}
