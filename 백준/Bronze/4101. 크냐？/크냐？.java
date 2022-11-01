90090a60434ecce733c40466f66f5f00d9cc22fd
ye <104442379+kookoo1029@users.noreply.github.com>
2022-11-02 02:44:02 +0900

    [Bronze V] Title: 크냐?, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.java
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String s = "";
-		while(true) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			if(a==0 && b==0) {
-				break;
-			}
-			s +=  a>b ? "Yes" + "\n" : "No" + "\n";
-		}
-		System.out.println(s);
-	}
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+        	//StringTokenizer 선언을  while 밖으로 빼면 오류남. 공백 단위로 읽을 수 있는 라인을 while문안에서 반복적으로 추가해야하기 때문
+        	StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+        	int a = Integer.parseInt(st.nextToken()); 
+        	int b = Integer.parseInt(st.nextToken()); 
+        	if(a==0 && b==0) {
+        		break;
+        	}
+        	sb.append(a>b ? "Yes" + "\n" :"No" + "\n");
+        }
+        System.out.println(sb);
+    }
 }
