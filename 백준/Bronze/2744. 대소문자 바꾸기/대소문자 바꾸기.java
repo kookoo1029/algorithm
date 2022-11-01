7d4c7cc47eb82b279307e54adc4efece4ed2814a
ye <104442379+kookoo1029@users.noreply.github.com>
2022-11-01 18:41:48 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 		for(int i=0; i<str.length(); i++) {
 			char c = str.charAt(i); // scanner는 char를 받을 수 없음. string을 받고 char로 변환
 			if(Character.isUpperCase(c)) {
@@ -12,5 +13,7 @@ public class Main {
 			}else
 				System.out.print(Character.toUpperCase(c));
 		}
-	}
+		
+        br.close();
+    }
 }
