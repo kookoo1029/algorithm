f09ddacca6bc52097f5f9625c97e9fba59e0e5c3
ye <104442379+kookoo1029@users.noreply.github.com>
2022-11-06 01:50:47 +0900

    [Bronze IV] Title: 세수정렬, Time: 228 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,23 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[3];
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();				
-		}
-		
-		for(int i=0; i<arr.length; i++) {
-			for(int j=i+1; j<arr.length; j++) {
-				if(arr[i]>arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		System.out.print(arr[i] + " ");
+		int[] arr = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+		Arrays.sort(arr);
+		for(int i : arr) {
+			System.out.print(i + " ");
 		}
 	}
 }
