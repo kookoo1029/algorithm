196d974c1f4c4ea74b7fb4bd1188bccbaa25f30b
ye <104442379+kookoo1029@users.noreply.github.com>
2022-11-09 20:12:58 +0900

    [Bronze IV] Title: 시험 점수, Time: 204 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Bronze/5596. 시험 점수/시험 점수.java
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -4,14 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
-		int e = sc.nextInt(), f = sc.nextInt(), g = sc.nextInt(), h = sc.nextInt();
-		int sum1 = a+b+c+d;
-		int sum2 = e+f+g+h;
-		if(sum2>sum1) {
-			System.out.println(sum2);
-		}else {
-			System.out.println(sum1);
+		int sum1 = 0;
+		int sum2 = 0;
+		for(int i=0; i<4; i++) {
+			sum1 += sc.nextInt();
 		}
+		for(int i=0; i<4; i++) {
+			sum2 += sc.nextInt();
+		}
+		System.out.println((sum2>sum1) ? sum2 : sum1);
 	}
 }
